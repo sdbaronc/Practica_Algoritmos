9290cdbd1e4580924046474f50487ae9dfe14eae
ARKAROK <henry.nunez.2019@upb.edu.co>
2019-02-25 20:58:29 -0500

    Sexto Algoritmo

---
+++ Calculadora.py
@@ -0,0 +1,18 @@
+print("Bienvenido a tu calculadora chichi")
+
+num1=float(input("Digita un numero, Por favor: "))
+num2=float(input("Digita un numero, Por favor: "))
+
+suma=num1+num2
+resta=num1-num2
+producto=num1*num2
+division=num1/num2
+residuo=num1%num2
+
+print("La suma de dos numeros es " + str(suma))
+print("La resta de dos numeros es " + str(resta))
+print("El producto de dos numeros es " + str(producto))
+print("La division de dos numeros es " + str(division))
+print("El residuo de dos numeros es " + str(residuo))
+
+print("Gracias por usar tu calculadora chichi")
