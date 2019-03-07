0e0fdc9c0d0d6e5786c41586e049bb38a1d4f079
ARKAROK <henry.nunez.2019@upb.edu.co>
2019-03-06 19:02:33 -0500

    Complemento de la parte 1

---
+++ Cordenadas.py
@@ -0,0 +1,14 @@
+import math
+
+print("Algoritmo que imprima cordenadas en un plano")
+
+x1=int(input("Digita un numero: "))
+y1=int(input("Digita un numero de nuevo: "))
+x2=int(input("Digita un numero de nuevo x2: "))
+y2=int(input("Digita un numero de nuevo x3: "))
+
+d=math.sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)))
+
+print("La distancia entre las cordenadas (x1,y1) y (x2,y2) es " + str(d))
+print("Final del programa")
+
---
+++ Nx11_nx111.py
@@ -0,0 +1,13 @@
+print("Algoritmo que lea un numero lo imprima y imprima dos y tres veces el mismo")
+
+numb=int(input("Digita un numero, Por favor: "))
+
+nn=numb*11
+nnn=numb*111
+suma=numb+nn+nnn
+
+print("El valor de n es " + str(numb))
+print("El valor de nn es " + str(nn))
+print("El valor de nnn es " + str(nnn))
+print("La suma de los numeros anteriores es " + str(suma))
+print("Final del programa")
