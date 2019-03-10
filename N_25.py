793e6ba4195633ec16b911bacaf265c19283f0b2
ARKAROK <henry.nunez.2019@upb.edu.co>
2019-03-09 21:34:05 -0500

    Casi que no :^)

--- N_25.py
+++ N_25.py
@@ -1,9 +1,14 @@
 print("Algoritmo que invierta numeros")
 
-numb=input("Digita el numero de 4 cifras que deseas invertir: ")
+a=int(input("Digita el numero de 4 cifras que deseas invertir: ")) #3245
 
-print(numb)
-print(numb[::-1])
-print("Final del programa")
+b=int(a/1000) #3
+c=a-(b*1000) #245
+d=int(c/100) #2
+e=c-(d*100) #45
+f=int(e/10) #4
+g=e-(f*10) #5
+
+print(str(g) + str(f) + str(d) + str(b))
 
 # numero 25
